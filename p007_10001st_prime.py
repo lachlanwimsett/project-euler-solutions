deb75486e1877b9b3481f618a43a385247a1ce64
Lachlan Wimsett <l.wimsett@gmail.com>
2021-05-08 18:23:08 +1200

    p007

--- p007_10001st_prime.py
+++ p007_10001st_prime.py
@@ -1,25 +1,34 @@
 from math import isqrt, log, ceil
 
 
+def bit_count(int_type):
+    count = 0
+    while int_type:
+        int_type &= int_type - 1
+        count += 1
+    return count
+
+
 def prime(n):
     limit = ceil(n * log(n) + n * log(log(n)))
     a = 0
     k = 0
-    i = 2
+    p = 2
     for i in range(2, isqrt(limit) + 1):
         a >>= 1
         if a & 1 == 0:
+            p = i
             k += 1
             b = 1 << (i * i - i)
             for _ in range(i * i, limit + 1, i):
                 a |= b
                 b <<= i
-    for i in range(i + 1, limit + 1):
-        a >>= 1
-        if a & 1 == 0:
-            k += 1
-            if k == n:
-                return i
+    a ^= (1 << a.bit_length()) - 1
+    primes = bit_count(a) + k
+
+    for _ in range(primes, n, -1):
+        a &= (1 << a.bit_length() - 1) - 1
+    return a.bit_length() + p
 
 
 def main():
