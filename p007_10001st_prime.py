5832848d25d7b7bb3d6f3d488c6796bc71932126
Lachlan Wimsett <l.wimsett@gmail.com>
2021-05-08 17:13:36 +1200

    p007

--- p007_10001st_prime.py
+++ p007_10001st_prime.py
@@ -1,27 +1,29 @@
-from math import isqrt
-from itertools import islice
+from math import isqrt, log, ceil
 
 
-def sieve(n):
+def prime(n):
+    limit = ceil(n * log(n) + n * log(log(n)))
     a = 0
+    k = 0
     i = 2
-    for i in range(2, isqrt(n) + 1):
+    for i in range(2, isqrt(limit) + 1):
         a >>= 1
         if a & 1 == 0:
-            yield i
-            i2 = i * i
-            b = 1 << (i2 - i)
-            for _ in range(i2, n + 1, i):
+            k += 1
+            b = 1 << (i * i - i)
+            for _ in range(i * i, limit + 1, i):
                 a |= b
                 b <<= i
     for i in range(i + 1, n + 1):
         a >>= 1
         if a & 1 == 0:
-            yield i
+            k += 1
+            if k == n:
+                return i
 
 
 def main():
-    print(next(islice(sieve(105_000), 10001 - 1, 10001)))
+    print(prime(10001))
 
 
 if __name__ == '__main__':
